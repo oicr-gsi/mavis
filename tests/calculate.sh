c2dc2cebe9a02fc055fd7b2517558b917e96cca4
iainrb <1695061+iainrb@users.noreply.github.com>
2021-01-26 18:28:53 -0500

    Modify calculate script to deal with non-deterministic filenames

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 cd $1
 
-find .  -name "*\.zip" -exec unzip {} \; >/dev/null
-find . -type f -not -path "./*.zip" | sort | xargs cat | md5sum
+find .  -name "*\.zip" -exec unzip {} \; >/dev/null # unzip the results files
+
+# filenames will vary, but contents should stay the same
+# approach 1:
+# find the md5 sums only and sort them
+
+echo "Method 1:"
+find . -type f -not -path "./*.zip" | xargs md5sum | cut -f 1 -d " " | sort
+
+# approach 2:
+# verify checksums of files with invariant names (eg. ./mavis_summary_all_WT.EPT0068.tab), only check for numbers of others (ie. total numbers of JSON/SVG files)
+
+echo "Method 2:"
+find . -name "\*.tab" | sort | xargs md5sum
+find . -name "\*.json" | xargs wc -l
+find . -name "\*.svg" | xargs wc -l
+
+# run test overnight and pick one
